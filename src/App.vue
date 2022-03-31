82414ec57fff9114f5ee028626bf743eedb5bde7
HanYongHui <xd66620147@163.com>
2022-03-31 20:37:58 +0800

    小程序 初始化

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <!--
- * @Description: 文件内容描述
+ * @Description: 小程序 主入口
  * @Author: HanYongHui
  * @Date: 2022-03-29 16:44:50
- * @LastEditTime: 2022-03-29 18:10:06
+ * @LastEditTime: 2022-03-31 18:08:48
  * @LastEditors: HanYongHui
 -->
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import { useUserInfoHooks } from "./hoosk/index";
 onLaunch(() => {
   console.log("App Launch");
+  const { storeData } = useUserInfoHooks();
+  try {
+    const token: string = uni.getStorageSync("token");
+    if (token) {
+      storeData.token = token;
+      uni.switchTab({ url: "/pages/home/index" });
+    }
+  } catch (e) {
+    console.log("获取token失败");
+  }
 });
-onShow(() => {
-  console.log("App Show");
-});
-onHide(() => {
-  console.log("App Hide");
-});
+onShow(() => {});
+onHide(() => {});
 </script>
-<style lang="scss"></style>
+<style >
+page {
+  height: 100%;
+  background-color: #f5f6f6;
+  font-size: 16px;
+}
+</style>
