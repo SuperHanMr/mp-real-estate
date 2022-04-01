750d7ca31b2ffd1b1630db5f4f155ab7c2e05132
李书梦 <2326107425@qq.com>
2022-04-01 10:46:18 +0800

    搭建需求相关页面

---
+++ src/pages/clue/scheme-details/scheme-details.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+		方案详情
+	</view>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="scss" scoped>
+</style>
