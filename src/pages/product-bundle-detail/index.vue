7fc4dc8fa37c7750be74eedb6e44c984e4661b2b
HanYongHui <xd66620147@163.com>
2022-05-24 10:23:22 +0800

    楼盘项目 图文详情页面开发

--- src/pages/product-bundle-detail/index.vue
+++ src/pages/product-bundle-detail/index.vue
@@ -2,7 +2,7 @@
  * @Description: 商品套包详情
  * @Author: HanYongHui
  * @Date: 2022-05-16 14:13:56
- * @LastEditTime: 2022-05-20 11:58:40
+ * @LastEditTime: 2022-05-24 10:18:27
  * @LastEditors: HanYongHui
 -->
 <template>
@@ -10,6 +10,9 @@
     <web-view
       :src="`${baseUrl}/app-pages/mp-real-estate/product-bundle.html?schemeId=${schemeId}&caseBagName=${caseBagName}`"
     />
+    <!-- <web-view
+      src="https://3d-mould.dabanjia.com/player2/preview/?modelId=169&time=1629959586853"
+    ></web-view> -->
   </view>
 </template>
 <script lang="ts" setup>
@@ -24,10 +27,6 @@ const props = defineProps({
 });
 console.log("props", props);
 const baseUrl = ref<string>(import.meta.env.VITE_URL_BASE_H5 as string);
-baseUrl.value = "https://localhost/";
-console.log(
-  `${baseUrl}/app-pages/mp-real-estate/product-bundle.html?schemeId=${props.schemeId}&caseBagName=${props.caseBagName}`
-);
 </script>
 <style lang="scss" scoped>
 .page-content {
